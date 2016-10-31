6fa52f529d9ede8d314d669121490efb8d494afb
Henk Bierlee <bierlee.henk@gmail.com>
2016-10-31 17:14:46 +0100

    Display chat messages in Chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,11 +2,38 @@ import React from 'react'
 import Contacts from './Contacts'
 
 class Chat extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.renderMessage = this.renderMessage.bind(this);
+	}
+
+
+	componentWillMount() {
+		const userId = this.props.params.userId
+
+		console.log("Start chat for " + userId)
+		
+		// init state
+		this.state = {
+			messages: ['a', 'b', 'c']
+		}
+	}
+
+	renderMessage(message, index) {
+		return (
+			<li key={index}>{message}</li>
+		)
+	}
+
 	render() {
 		return (
-			<div className="chat">
+			<div className="Chat">
 				<h1>Welcome, {this.props.params.userId}</h1>
 			
+				<ul className="Chat-messages">
+					{this.state.messages.map(this.renderMessage)}
+				</ul>
 				<Contacts />
 			</div>
 		)
