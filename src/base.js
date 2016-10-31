48875dfabed27e6d451e4bd1e871de5faf58a412
Henk Bierlee <bierlee.henk@gmail.com>
2016-10-31 17:55:00 +0100

    Install re-base and connect to Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyBBw9885ZMydgfDmh-eyJCnR6MwLywuv7Y",
+	authDomain: "react-chat-app-e31a5.firebaseapp.com",
+	databaseURL: "https://react-chat-app-e31a5.firebaseio.com",
+})
+
+export default base
