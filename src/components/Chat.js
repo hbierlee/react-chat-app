2570d79f05f7a047df8251f0d00d951f1f546e89
Henk Bierlee <bierlee.henk@gmail.com>
2016-10-31 17:59:14 +0100

    Sync users state to db and check if requested user exists

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,23 +1,51 @@
 import React from 'react'
 import Contacts from './Contacts'
+import base from '../base'
 
 class Chat extends React.Component {
 	constructor(props) {
 		super(props)
 
-		this.renderMessage = this.renderMessage.bind(this);
+		this.renderMessage = this.renderMessage.bind(this)
+		this.createUser = this.createUser.bind(this)
+		this.checkForUser = this.checkForUser.bind(this)
+
+		// init state
+		this.state = {
+			messages: ['a', 'b', 'c'],
+		}
 	}
 
+	createUser(user) {
+		const users = {...this.state.users}
+		users[user.userId] = user
+		this.setState({users})
+	}
 
 	componentWillMount() {
 		const userId = this.props.params.userId
 
 		console.log("Start chat for " + userId)
-		
-		// init state
-		this.state = {
-			messages: ['a', 'b', 'c']
+
+		this.ref = base.syncState(`users/`, {
+			context: this,
+			state: 'users',
+			then() {this.checkForUser(userId)},
+    	})
+	}
+
+	checkForUser(userId) {
+		if (this.state.users.hasOwnProperty(userId)) {
+			console.log(`User ${userId} found`)
 		}
+		else {
+			console.log(`Add new user ${userId}`)
+			this.createUser({userId})
+		}
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref)
 	}
 
 	renderMessage(message, index) {
@@ -34,7 +62,7 @@ class Chat extends React.Component {
 				<ul className="Chat-messages">
 					{this.state.messages.map(this.renderMessage)}
 				</ul>
-				<Contacts />
+				<Contacts/>
 			</div>
 		)
 	}
