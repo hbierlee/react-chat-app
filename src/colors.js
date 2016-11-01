6dd4e5bf739e01fa31402f2f7843650dbbb21cc4
Henk Bierlee <bierlee.henk@gmail.com>
2016-11-01 15:18:40 +0100

    Pick color scheme

---
+++ src/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+	color1: "rgba(34, 46, 80, 1)",
+	color2: "rgba(0, 121, 145, 1)",
+	color3: "rgba(67, 154, 134, 1)",
+	color4: "rgba(188, 216, 193, 1)",
+	color5: "rgba(233, 217, 133, 1)",
+}
